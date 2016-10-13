0e0f8d492cff3382d3d62cde1fa29779dbff16fd
rishabh <rishabh999gupta@gmail.com>
2016-10-13 16:48:49 +0530

    altered main.m

--- Code/main.m
+++ Code/main.m
@@ -1,15 +1,20 @@
  
  
-dataset_orig=xlsread('NSE-CIPLA.xls');
+dataset_orig=xlsread('NSE-CIPLA.xlsx');
 dataset=normalize(dataset_orig);
-GD=1;  % GD=1 is BAtch   % GD=0 is stochastic
+GD=0;  % GD=1 is Batch   % GD=0 is stochastic
+alpha=0.0001;
+delta=0;
+percent=0.7;
 
 %[parameters,prediction,MSE] = Linear_Regression(dataset, percent,alpha, delta,GD)
-[parameters,prediction,MSE]=Linear_Regression(dataset,0.7,0.01,0,GD);
+[parameters,prediction,MSE]=Linear_Regression(dataset,percent,alpha, delta,GD);
 
  disp(parameters);
  %disp(prediction);
  disp(MSE);
+ 
+ 
     
  
   
