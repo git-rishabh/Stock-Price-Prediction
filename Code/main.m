cbf73312aa7666a547143e1bf292f03b2956c5ac
kushagramahajan <kushagra14055@iiitd.ac.in>
2016-10-16 09:05:49 +0530

    he

--- Code/main.m
+++ Code/main.m
@@ -9,9 +9,9 @@ percent=0.7;
 p=2;
 
 %[parameters,prediction,MSE] = Linear_Regression(dataset, percent,alpha, delta,GD)
-[parameters,prediction,MSE]=Linear_Regression(dataset,percent,alpha, delta,p,GD);
+%[parameters,prediction,MSE]=Linear_Regression(dataset,percent,alpha, delta,p,GD);
 %[prediction,MSE]=Gaussian_regression(dataset,percent,alpha, delta,p,GD);
-
+[prediction,MSE]=SVR(dataset,percent,alpha, delta,p,GD);
  %disp(parameters);
  %disp(prediction);
  disp(MSE);
