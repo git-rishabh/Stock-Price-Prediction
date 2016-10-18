f3b73122e3435d256f5a714f122b5ab6cbe1fc0d
kushagramahajan <kushagra14055@iiitd.ac.in>
2016-10-18 20:08:04 +0530

    cv files done

--- Code/main.m
+++ Code/main.m
@@ -1,11 +1,11 @@
  
-dataset_orig=csvread('Datasets\NSE-CIPLA_new.csv');
+dataset_orig=csvread('Datasets\NSE-TCS_new.csv');
 dataset=normalize(dataset_orig);
 GD=0;  % GD=1 is Batch   % GD=0 is stochastic
 alpha=0.1;
 delta=0;
 percent=0.8;
-p=1;
+p=2;
 
 dvalues=[0,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00005,0.00001];
 avalues=[0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00005,0.00001];
